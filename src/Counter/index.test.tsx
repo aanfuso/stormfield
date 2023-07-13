f47ad0df7220826d8ac6f037580d5dc89b1fbe91
Agustin Anfuso <anfusoagustin89@gmail.com>
2023-07-13 15:20:43 +0400

    Add Counter + unit tests

---
+++ src/Counter/index.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Counter from './index';
+
+beforeEach(() => {
+  render(<Counter />)
+})
+
+test('renders a counter', () => {
+  const counter = screen.getByTestId('counter');
+
+  expect(counter).toContainHTML('0');
+});
+
+test('increase counter when + is clicked', () => {
+  const button = screen.getByTestId('increase-button');
+  const counter = screen.getByTestId('counter');
+
+  act(() => {
+    userEvent.click(button);
+  });
+
+  expect(counter).toContainHTML('1');
+});
+
+test('increase counter when - is clicked', () => {
+  const button = screen.getByTestId('decrease-button');
+  const counter = screen.getByTestId('counter');
+
+  act(() => {
+    userEvent.click(button);
+  });
+
+  expect(counter).toContainHTML('-1');
+});
---
+++ src/Counter/index.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import {
+  Button,
+  Paper,
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  flexGrow: 1,
+}));
+
+type CounterProps = {
+  type?: string;
+  min?: number;
+} & typeof defaultProps;
+
+const defaultProps = {
+  min: 0,
+};
+
+function Counter({ type } : CounterProps) {
+  const [count, setCount] = useState(0)
+
+  const handleIncrease = () => {
+    setCount(count + 1)
+  }
+
+  const handleDecrease = () => {
+    setCount(count - 1)
+  }
+
+  const counterText = type ? `${type}: ${count}` : count;
+
+  return (
+    <>
+      <Button
+        onClick={handleDecrease}
+        data-testid='decrease-button'
+      >
+        -1
+      </Button>
+      <Item data-testid='counter'>
+        {counterText}
+      </Item>
+      <Button
+        onClick={handleIncrease}
+        data-testid='increase-button'
+      >
+        +1
+      </Button>
+    </>
+  );
+}
+
+Counter.defaultProps = defaultProps;
+
+export default Counter;
