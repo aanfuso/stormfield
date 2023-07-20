50c7e18ee5f38e7214ccde49b0eeb89c43424c2c
Agustin Anfuso <anfusoagustin89@gmail.com>
2023-07-20 13:50:57 +0400

    Change counters order

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,9 +1,9 @@
 export const DEFAULT_COUNTERS = [
-  { type: 'storm', symbol: '' },
   { type: 'white', symbol: '{W}' },
   { type: 'blue', symbol: '{U}' },
   { type: 'black', symbol: '{B}' },
   { type: 'red', symbol: '{R}' },
   { type: 'green', symbol: '{G}' },
   { type: 'colorless', symbol: '{C}' },
+  { type: 'storm', symbol: '' },
 ];
