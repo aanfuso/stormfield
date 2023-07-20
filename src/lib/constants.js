c8996da32c2aa1a45dcac1bb2da8e33fdfa48090
Agustin Anfuso <anfusoagustin89@gmail.com>
2023-07-20 19:25:01 +0400

    Add count to default counters

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,9 +1,9 @@
 export const DEFAULT_COUNTERS = [
-  { label: 'White', type: 'white', symbol: '{W}' },
-  { label: 'Blue', type: 'blue', symbol: '{U}' },
-  { label: 'Black', type: 'black', symbol: '{B}' },
-  { label: 'Red', type: 'red', symbol: '{R}' },
-  { label: 'Green', type: 'green', symbol: '{G}' },
-  { label: 'Colorless', type: 'colorless', symbol: '{C}' },
-  { label: 'Storm', type: 'storm', symbol: '' },
+  { count: 0, label: 'White', type: 'white', symbol: '{W}' },
+  { count: 0, label: 'Blue', type: 'blue', symbol: '{U}' },
+  { count: 0, label: 'Black', type: 'black', symbol: '{B}' },
+  { count: 0, label: 'Red', type: 'red', symbol: '{R}' },
+  { count: 0, label: 'Green', type: 'green', symbol: '{G}' },
+  { count: 0, label: 'Colorless', type: 'colorless', symbol: '{C}' },
+  { count: 0, label: 'Storm', type: 'storm', symbol: '' },
 ];
