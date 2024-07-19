3866f0835f43b75cd56daf2069b3b2d380a8a0c2
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-07-19 22:27:08 +0400

    Simplify default counters constant

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,9 +1,9 @@
 export const DEFAULT_COUNTERS = [
-  { count: 0, label: 'White', type: 'white', symbol: '{W}' },
-  { count: 0, label: 'Blue', type: 'blue', symbol: '{U}' },
-  { count: 0, label: 'Black', type: 'black', symbol: '{B}' },
-  { count: 0, label: 'Red', type: 'red', symbol: '{R}' },
-  { count: 0, label: 'Green', type: 'green', symbol: '{G}' },
-  { count: 0, label: 'Colorless', type: 'colorless', symbol: '{C}' },
-  { count: 0, label: 'Storm', type: 'storm', symbol: '' },
+  { count: 0, label: 'White', type: 'white' },
+  { count: 0, label: 'Blue', type: 'blue' },
+  { count: 0, label: 'Black', type: 'black' },
+  { count: 0, label: 'Red', type: 'red' },
+  { count: 0, label: 'Green', type: 'green' },
+  { count: 0, label: 'Colorless', type: 'colorless' },
+  { count: 0, label: 'Storm', type: 'storm' },
 ];
