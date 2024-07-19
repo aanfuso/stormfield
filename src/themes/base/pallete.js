4ff1244473528c2ebe7af03b880b28fb97e403f5
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-07-19 19:54:30 +0400

    Switch to dark mode and change pallete

--- src/themes/base/pallete.js
+++ src/themes/base/pallete.js
@@ -1,47 +1,13 @@
 const primary = {
-  main: 'hsl(170, 48%, 47%)',
-  light: 'hsl(170, 48%, 57%)',
-};
-
-const secondary = {
   main: 'hsl(256, 49%, 47%)',
 };
 
-const whatsapp = {
-  main: 'hsl(142, 70%, 48%)',
-};
-
-const background = {
-  default: "hsl(30, 8%, 95%)",
-};
-
-const blue = {
-  100: '#DAECFF',
-  200: '#80BFFF',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
-  700: '#0059B2',
-};
-
-const grey = {
-  50: '#F3F6F9',
-  100: '#E5EAF2',
-  200: '#DAE2ED',
-  300: '#C7D0DD',
-  400: '#B0B8C4',
-  500: '#9DA8B7',
-  600: '#6B7A90',
-  700: '#434D5B',
-  800: '#303740',
-  900: '#1C2025',
+const secondary = {
+  main: 'hsl(256, 49%, 67%)',
 };
 
 export const palette = {
+  mode: 'dark',
   primary,
   secondary,
-  background,
-  whatsapp,
-  grey,
-  blue,
 };
